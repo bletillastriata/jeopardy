c0e56f0544c6b77c45fd90da87b715f9d1a92857
bletillastriata <sulan.mair@gmail.com>
2019-11-03 16:24:03 -0500

    Update views.py

--- jeopardy/views.py
+++ jeopardy/views.py
@@ -2,14 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Clue
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import ClueSearchForm, BasicSearchForm
+from .forms import ClueSearchForm
 from django.db.models import Q
 import datetime
-
-
-
-
-# Create your views here.
 
 
 def home(request):
@@ -23,9 +18,7 @@ def home(request):
 	return render(request, 'home.html',{'clues':clues, 'form':form})
 
 def clue_topics(request, pk):
-# clue = Clue.objects.get(pk=pk)
 	clue = get_object_or_404(Clue, pk=pk)
-# return render(request, 'bleh.html')
 	return render(request, 'clues.html', {'clue':clue})
 
 def search(request):
@@ -43,6 +36,7 @@ def search(request):
 				)
 			print("data was", form.cleaned_data)
 
+			# filtering by date(s)
 			if form.cleaned_data['start_date'] != '' and form.cleaned_data['end_date'] != '':
 				start_date = form.cleaned_data['start_date']
 				start_date = datetime.datetime.strptime(start_date, '%M-%d-%Y').date()
@@ -53,8 +47,12 @@ def search(request):
 				start_date = form.cleaned_data['start_date']
 				start_date = datetime.datetime.strptime(start_date, '%M-%d-%Y').date()
 				clues_list = clues_list.filter(airdate__range=[start_date, datetime.date.today()])
+			
+			# filtering by value (difficulty)
 			if form.cleaned_data['value'] != 'any':
 				clues_list = clues_list.filter(value__exact=form.cleaned_data['value'])
+			
+			# filtering by category
 			if form.cleaned_data['category'] != '':
 				clues_list = clues_list.filter(category_name__exact=form.cleaned_data['category'])
 			
@@ -66,35 +64,3 @@ def search(request):
 		form = ClueSearchForm()
 		return render(request, 'search.html', {'form': form})
 
-def search_results(request):
-	form = ClueSearchForm(request.POST)
-	print(form.is_valid())
-
-	if form.is_valid():
-		keyword = form.cleaned_data['keyword']
-		clues_list = Clue.objects.filter(
-			Q(question__icontains=keyword) |
-			Q(answer__icontains=keyword) |
-			Q(category__icontains=keyword)	
-			)
-		print("data was", form.cleaned_data)
-
-		if form.cleaned_data['start_date'] != '' and form.cleaned_data['end_date'] != '':
-			start_date = form.cleaned_data['start_date']
-			start_date = datetime.datetime.strptime(start_date, '%M-%d-%Y').date()
-			end_date = form.cleaned_data['end_date']
-			end_date = datetime.datetime.strptime(end_date, '%M-%d-%Y').date()
-			clues_list = clues_list.filter(airdate__range=[start_date, end_date])
-		if form.cleaned_data['start_date'] != '':
-			start_date = form.cleaned_data['start_date']
-			start_date = datetime.datetime.strptime(start_date, '%M-%d-%Y').date()
-			clues_list = clues_list.filter(airdate__range=[start_date, datetime.date.today()])
-
-		clues_list = clues_list.order_by('airdate')
-
-		paginator = Paginator(clues_list, 50)
-		page = request.GET.get('page')
-		clues = paginator.get_page(page)
-		print('got here')
-
-		return render(request, 'searchresults.html',{'clues':clues})
